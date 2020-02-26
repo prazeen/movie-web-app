96a8028d488f60ccf25254292a6d66de1340f3a4
Prajin <shresthaprazeen@gmail.com>
2020-02-26 11:28:41 +0545

    movie

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./routing/Home";
+import AboutPage from "./routing/About";
+import Product from "./routing/Product";
+import Services from "./routing/Services";
+import PageNotFound from "./routing/404";
+import Navbar from "./routing/Navbar";
 
-class App extends Component {
+export default class App extends Component {
   render() {
-    return <h1>App js</h1>;
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/product" component={Product} />
+        <Route path="/services" component={Services} />
+        <Route path="page-not-found" component={PageNotFound} />
+      </React.Fragment>
+    );
   }
 }
-
-export default App;
--- src/routing/Routing.js
+++ src/routing/Routing.js
@@ -1,37 +1,37 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
-import HomePage from "./Home";
-import AboutPage from "./About";
-import Services from "./Services";
-import Navbar from "./Navbar";
-import PageNotFound from "./404";
-import Product from "./Product";
-class Routing extends Component {
-  state = {
-    isAuthenticated: false
-  };
-  render() {
-    if (this.state.isAuthenticated) {
-      return <Redirect to="/dashboard" />;
-    }
-    return (
-      <React.Fragment>
-        <Navbar />
-        <Switch>
-          <Route
-            path="/"
-            exact
-            render={props => <HomePage {...props} data="dhbdhd" />}
-          />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/services" exact component={Services} />
-          <Route path="/services/:id" component={Product} />
-          <Route path="/page-not-found" component={PageNotFound} />
-          <Redirect to="/page-not-found" />
-        </Switch>
-      </React.Fragment>
-    );
-  }
-}
+// import React, { Component } from "react";
+// import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+// import HomePage from "./Home";
+// import AboutPage from "./About";
+// import Services from "./Services";
+// import Navbar from "./Navbar";
+// import PageNotFound from "./404";
+// import Product from "./Product";
+// class Routing extends Component {
+//   state = {
+//     isAuthenticated: false
+//   };
+//   render() {
+//     if (this.state.isAuthenticated) {
+//       return <Redirect to="/dashboard" />;
+//     }
+//     return (
+//       <React.Fragment>
+//         <Navbar />
+//         <Switch>
+//           <Route
+//             path="/"
+//             exact
+//             render={props => <HomePage {...props} data="dhbdhd" />}
+//           />
+//           <Route path="/about" component={AboutPage} />
+//           <Route path="/services" exact component={Services} />
+//           <Route path="/services/:id" component={Product} />
+//           <Route path="/page-not-found" component={PageNotFound} />
+//           <Redirect to="/page-not-found" />
+//         </Switch>
+//       </React.Fragment>
+//     );
+//   }
+// }
 
-export default Routing;
+// export default Routing;
